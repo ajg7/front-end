2e3501b1242fe445a60fbc17076f2597f074d1e6
ajg7 <ajgebara@gmail.com>
2020-08-24 12:24:33 -0500

    made skeleton of login inputs

--- better-professor-app/src/components/Form.js
+++ better-professor-app/src/components/Form.js
@@ -5,6 +5,28 @@ const Form = () => {
 
     return(
         <>
+            <form>
+                <label> Name:
+                    <input 
+                    type="text"
+                    />                
+                </label>
+                <label> Email:
+                    <input 
+                    type="email"
+                    />
+                </label>
+                <label> Class Name: 
+                    <input 
+                    type="text"
+                    />  
+                </label>
+                <label> Class Section: 
+                    <input 
+                    type="text"
+                    />  
+                </label>
+            </form>
         </>
     )
 }
