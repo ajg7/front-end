12e4ca79a5e9d141f6f29ce69b88b61106253356
ajg7 <ajgebara@gmail.com>
2020-08-27 10:53:27 -0500

    put styled login form into the theme folder

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -3,35 +3,6 @@ import { Link } from "react-router-dom";
 import Form from "./Form.js";
 import styled from "styled-components";
 
-const StyledLoginPage = styled.div`
-
-    div {
-        border: 2px solid black;
-        padding: 10px 10px;
-
-        &:hover {
-            background-color: ${({ theme }) => theme.secondaryColor};
-            h2 {
-                color: ${({ theme }) => theme.primaryColor};
-            }
-        }
-    }
-
-    button {
-        border: 4px solid ${({ theme }) => theme.primaryColor};
-        border-radius: 20px;
-        color: ${({ theme }) => theme.primaryColor};
-        background-color: ${({ theme }) => theme.offWhite};
-        font-weight: bold;
-
-        &:hover {
-            color: ${({ theme }) => theme.offWhite};
-            background-color: ${({ theme }) => theme.primaryColor};
-            transition: all 0.3s ease-in-out;
-        }
-    }
-`
-
 const LoginPage = props => {
     return(
         <>
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 const theme = {
     primaryColor: "#3571A7",
     secondaryColor: "#FFC500",
@@ -6,3 +8,32 @@ const theme = {
 }
 
 export default theme;
+
+export const StyledForm = styled.div`
+
+    div {
+        border: 2px solid black;
+        padding: 10px 10px;
+
+        &:hover {
+            background-color: ${ theme => theme.secondaryColor};
+            h2 {
+                color: ${theme => theme.primaryColor};
+            }
+        }
+    }
+
+    button {
+        border: 4px solid ${theme => theme.primaryColor};
+        border-radius: 20px;
+        color: ${theme => theme.primaryColor};
+        background-color: ${theme => theme.offWhite};
+        font-weight: bold;
+
+        &:hover {
+            color: ${theme => theme.offWhite};
+            background-color: ${theme => theme.primaryColor};
+            transition: all 0.3s ease-in-out;
+        }
+    }
+`
