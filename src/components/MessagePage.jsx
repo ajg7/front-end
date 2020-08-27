c8257423c1d3f306cdcad88dc15ab0631022f4ea
ZaveDev <66022270+ZaveDev@users.noreply.github.com>
2020-08-27 11:31:22 -0400

    merging

--- src/components/MessagePage.jsx
+++ src/components/MessagePage.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 const MessagePage = () => {
   return (
     <>
-      <div>
+      <div className='message-card'>
         <h2>Message Title</h2>
         <p>who it is meant for</p>
         <p>time and date</p>
         <p>The message</p>
+        <button>delete</button>
+        <button>edit</button>
       </div>
       
     </>
